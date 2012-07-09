7bbc3b8d31b9c1dc9c0c1f519f6e2240080282e1
Stephan Leicht <Stephan.Leicht@bsiag.com>
2012-07-09 06:07:10 +0000

    made it work in eclipse 3.7 / swt

--- org.eclipse.scout.rt.demo.ui.swt/src/org/eclipse/scout/rt/demo/ui/swt/application/Application.java
+++ org.eclipse.scout.rt.demo.ui.swt/src/org/eclipse/scout/rt/demo/ui/swt/application/Application.java
@@ -11,17 +11,18 @@
 package org.eclipse.scout.rt.demo.ui.swt.application;
 
 import java.security.PrivilegedExceptionAction;
+
 import javax.security.auth.Subject;
+
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.eclipse.scout.net.NetActivator;
 import org.eclipse.scout.commons.security.SimplePrincipal;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
-/** <h3>Activator</h3>
- *  This class controls all aspects of the application's execution
+/**
+ * <h3>Activator</h3> This class controls all aspects of the application's execution
  */
 public class Application implements IApplication {
 
@@ -39,7 +40,7 @@ public class Application implements IApplication {
 
   public Integer startSecure(final IApplicationContext context) throws Exception {
     Display display = PlatformUI.createDisplay();
-    NetActivator.install();
+//    NetActivator.install();
     if (PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor()) == PlatformUI.RETURN_RESTART) {
       return EXIT_RESTART;
     }
@@ -55,8 +56,8 @@ public class Application implements IApplication {
       @Override
       public void run() {
         if (!display.isDisposed())
-          workbench.close();
-        }
+        workbench.close();
+      }
     });
   }
 }
