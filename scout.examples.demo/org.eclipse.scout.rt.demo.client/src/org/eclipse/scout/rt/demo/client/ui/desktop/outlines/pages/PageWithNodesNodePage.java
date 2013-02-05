72d1da7b5e648b3e099c64d3febd4927f7234e69
Sandro Schwager <sandro.schwager@bsiag.com>
2013-02-05 13:02:48 +0100

    Added copyright header to all java-files Deleted unused server files

--- scout.examples.demo/org.eclipse.scout.rt.demo.client/src/org/eclipse/scout/rt/demo/client/ui/desktop/outlines/pages/PageWithNodesNodePage.java
+++ scout.examples.demo/org.eclipse.scout.rt.demo.client/src/org/eclipse/scout/rt/demo/client/ui/desktop/outlines/pages/PageWithNodesNodePage.java
@@ -1,3 +1,14 @@
+/********************************************************************import java.util.Collection;
+
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.AbstractPageWithNodes;
+import org.eclipse.scout.rt.client.ui.desktop.outline.pages.IPage;
+import org.eclipse.scout.rt.shared.TEXTS;
+eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.scout.rt.demo.client.ui.desktop.outlines.pages;
 
 import java.util.Collection;
--- scout.examples.demo/org.eclipse.scout.rt.demo.shared/src/org/eclipse/scout/rt/demo/shared/services/outline/IStandardOutlineService.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.scout.rt.demo.shared.services.outline;
-
-import org.eclipse.scout.service.IService2;
-import org.eclipse.scout.commons.exception.ProcessingException;
-
-public interface IStandardOutlineService extends IService2{
-
-  public Object[][] getPageWithADetailformTableData() throws ProcessingException;
-}
