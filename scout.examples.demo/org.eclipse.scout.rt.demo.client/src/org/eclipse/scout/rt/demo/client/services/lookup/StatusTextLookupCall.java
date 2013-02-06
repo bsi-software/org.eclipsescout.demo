f2a7a0b95b02f998315de380d2af6b991fd46738
Sandro Schwager <sandro.schwager@bsiag.com>
2013-02-06 14:53:43 +0100

    Small error deleted

--- scout.examples.demo/org.eclipse.scout.rt.demo.client/src/org/eclipse/scout/rt/demo/client/services/lookup/StatusTextLookupCall.java
+++ scout.examples.demo/org.eclipse.scout.rt.demo.client/src/org/eclipse/scout/rt/demo/client/services/lookup/StatusTextLookupCall.java
@@ -16,9 +16,9 @@ public class StatusTextLookupCall extends LocalLookupCall {
   @Override
   protected List<LookupRow> execCreateLookupRows() throws ProcessingException {
     ArrayList<LookupRow> rows = new ArrayList<LookupRow>();
-    rows.add(new LookupRow("Fatal", TEXTS.get("Fatal"), "status_error", null, "000000", "ffffff", new FontSpec("Courir", FontSpec.STYLE_BOLD + FontSpec.STYLE_ITALIC, 12)));
-    rows.add(new LookupRow("Error", TEXTS.get("Error0"), "status_error", null, "ff0000", "000000", new FontSpec("Courir", FontSpec.STYLE_BOLD, 12)));
-    rows.add(new LookupRow("Warning", TEXTS.get("Warning"), "status_warning", null, "ffff00", "000000", new FontSpec("Courir", FontSpec.STYLE_ITALIC, 12)));
+    rows.add(new LookupRow(1L, TEXTS.get("Fatal"), "status_error", null, "000000", "ffffff", new FontSpec("Courir", FontSpec.STYLE_BOLD + FontSpec.STYLE_ITALIC, 12)));
+    rows.add(new LookupRow(2L, TEXTS.get("Error0"), "status_error", null, "ff0000", "000000", new FontSpec("Courir", FontSpec.STYLE_BOLD, 12)));
+    rows.add(new LookupRow(3L, TEXTS.get("Warning"), "status_warning", null, "ffff00", "000000", new FontSpec("Courir", FontSpec.STYLE_ITALIC, 12)));
     return rows;
   }
 }
