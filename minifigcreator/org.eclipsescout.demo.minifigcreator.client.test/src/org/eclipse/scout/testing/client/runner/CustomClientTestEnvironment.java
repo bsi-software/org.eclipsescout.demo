34803201dcc898037195981357d4c29b215bb851
Stephan Leicht Vogt <stephan.leicht@bsiag.com>
2013-03-19 20:07:17 +0100

    update platform.target to kepler

--- minifigcreator/org.eclipsescout.demo.minifigcreator.client.test/src/org/eclipse/scout/testing/client/runner/CustomClientTestEnvironment.java
+++ minifigcreator/org.eclipsescout.demo.minifigcreator.client.test/src/org/eclipse/scout/testing/client/runner/CustomClientTestEnvironment.java
@@ -1,17 +1,15 @@
 package org.eclipse.scout.testing.client.runner;
 
-
 import org.eclipsescout.demo.minifigcreator.client.ClientSession;
 
 public class CustomClientTestEnvironment implements IClientTestEnvironment {
 
-  @Override
-  public void setupGlobalEnvironment() {
-    ScoutClientTestRunner.setDefaultClientSessionClass(ClientSession.class);
-  }
+	@Override
+	public void setupGlobalEnvironment() {
+		ScoutClientTestRunner.setDefaultClientSessionClass(ClientSession.class);
+	}
 
-  @Override
-  public void setupInstanceEnvironment() {
-  }
-
+	@Override
+	public void setupInstanceEnvironment() {
+	}
 }
--- minifigcreator/org.eclipsescout.demo.minifigcreator.shared.test/src/org/eclipsescout/demo/minifigcreator/shared/minifig/part/PartUtilityTest.java
+++ minifigcreator/org.eclipsescout.demo.minifigcreator.shared.test/src/org/eclipsescout/demo/minifigcreator/shared/minifig/part/PartUtilityTest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.eclipsescout.demo.minifigcreator.shared.minifig.part;
 
@@ -10,7 +10,6 @@ import org.junit.Test;
 
 /**
  * @author jbr
- *
  */
 public class PartUtilityTest {
 	private static final String NAME1 = "Alice";
@@ -21,53 +20,52 @@ public class PartUtilityTest {
 	private static final int VALUE1 = 23;
 	private static final int VALUE2 = 42;
 	private static final int VALUE3 = 65;
-	private static final int VALUE_SUM = VALUE1+VALUE2+VALUE3;
-	
+	private static final int VALUE_SUM = VALUE1 + VALUE2 + VALUE3;
+
 	private static final Part PART1 = new Part(1, PART_NAME1, VALUE1);
 	private static final Part PART2 = new Part(2, PART_NAME2, VALUE2);
 	private static final Part PART3 = new Part(3, PART_NAME3, VALUE3);
 	private static final Part PART4 = new Part(89, "Part_Name10", VALUE1);
-	
-	private static final String EXPECTED_PARTS_AND_VALUE_ALL = " ["+PART_NAME1+", "+PART_NAME2+", "+PART_NAME3+"] - value: "+ VALUE_SUM;
-	private static final String EXPECTED_PARTS_AND_VALUE_1_AND_3 = " ["+PART_NAME1+", "+PART_NAME3+"] - value: "+ (VALUE1+VALUE3);
-	private static final String EXPECTED_PARTS_AND_VALUE_1_AND_2 = " ["+PART_NAME1+", "+PART_NAME2+"] - value: "+ (VALUE1+VALUE2);
-	private static final String EXPECTED_PARTS_AND_VALUE_2_AND_3 = " ["+PART_NAME2+", "+PART_NAME3+"] - value: "+ (VALUE2+VALUE3);
 
-	
+	private static final String EXPECTED_PARTS_AND_VALUE_ALL = " [" + PART_NAME1 + ", " + PART_NAME2 + ", " + PART_NAME3 + "] - value: " + VALUE_SUM;
+	private static final String EXPECTED_PARTS_AND_VALUE_1_AND_3 = " [" + PART_NAME1 + ", " + PART_NAME3 + "] - value: " + (VALUE1 + VALUE3);
+	private static final String EXPECTED_PARTS_AND_VALUE_1_AND_2 = " [" + PART_NAME1 + ", " + PART_NAME2 + "] - value: " + (VALUE1 + VALUE2);
+	private static final String EXPECTED_PARTS_AND_VALUE_2_AND_3 = " [" + PART_NAME2 + ", " + PART_NAME3 + "] - value: " + (VALUE2 + VALUE3);
+
 	/**
-	 * Test method for {@link org.eclipsescout.demo.minifigcreator.shared.minifig.part.PartUtility#calculateSummary(org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part, org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part, org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part)}.
+	 * Test method for {@link PartUtility#calculateSummary(Part, Part, Part)} .
 	 */
 	@Test
 	public void testCalculateSummary() {
 		Assert.assertEquals("Summary", NAME1 + EXPECTED_PARTS_AND_VALUE_ALL, PartUtility.calculateSummary(NAME1, PART1, PART2, PART3));
 		Assert.assertEquals("Summary", NAME2 + EXPECTED_PARTS_AND_VALUE_ALL, PartUtility.calculateSummary(NAME2, PART1, PART2, PART3));
 		Assert.assertEquals("Summary", "..." + EXPECTED_PARTS_AND_VALUE_ALL, PartUtility.calculateSummary(null, PART1, PART2, PART3));
-		
+
 		Assert.assertEquals("Summary", NAME1 + EXPECTED_PARTS_AND_VALUE_2_AND_3, PartUtility.calculateSummary(NAME1, null, PART2, PART3));
 		Assert.assertEquals("Summary", NAME1 + EXPECTED_PARTS_AND_VALUE_1_AND_3, PartUtility.calculateSummary(NAME1, PART1, null, PART3));
 		Assert.assertEquals("Summary", NAME1 + EXPECTED_PARTS_AND_VALUE_1_AND_2, PartUtility.calculateSummary(NAME1, PART1, PART2, null));
 		Assert.assertEquals("Summary", NAME2 + EXPECTED_PARTS_AND_VALUE_2_AND_3, PartUtility.calculateSummary(NAME2, null, PART2, PART3));
 		Assert.assertEquals("Summary", "..." + EXPECTED_PARTS_AND_VALUE_1_AND_2, PartUtility.calculateSummary(null, PART1, PART2, null));
-		
+
 		Assert.assertEquals("Summary", NAME1 + " - value: 0", PartUtility.calculateSummary(NAME1, null, null, null));
 		Assert.assertEquals("Summary", NAME2 + " - value: 0", PartUtility.calculateSummary(NAME2, null, null, null));
 		Assert.assertEquals("Summary", "... - value: 0", PartUtility.calculateSummary(null, null, null, null));
 	}
 
 	/**
-	 * Test method for {@link org.eclipsescout.demo.minifigcreator.shared.minifig.part.PartUtility#calculateValue(org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part, org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part, org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part)}.
+	 * Test method for {@link PartUtility#calculateValue(Part, Part, Part)} .
 	 */
 	@Test
 	public void testCalculateValue() {
 		Assert.assertEquals("Value", VALUE_SUM, PartUtility.calculateValue(PART1, PART2, PART3));
-		Assert.assertEquals("Value", VALUE2+VALUE3, PartUtility.calculateValue(null, PART2, PART3));
-		Assert.assertEquals("Value", VALUE1+VALUE3, PartUtility.calculateValue(PART1, null, PART3));
-		Assert.assertEquals("Value", VALUE1+VALUE2, PartUtility.calculateValue(PART1, PART2, null));
+		Assert.assertEquals("Value", VALUE2 + VALUE3, PartUtility.calculateValue(null, PART2, PART3));
+		Assert.assertEquals("Value", VALUE1 + VALUE3, PartUtility.calculateValue(PART1, null, PART3));
+		Assert.assertEquals("Value", VALUE1 + VALUE2, PartUtility.calculateValue(PART1, PART2, null));
 		Assert.assertEquals("Value", 0, PartUtility.calculateValue(null, null, null));
 	}
 
 	/**
-	 * Test method for {@link org.eclipsescout.demo.minifigcreator.shared.minifig.part.PartUtility#calculateImageId(org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part, org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part, org.eclipsescout.demo.minifigcreator.shared.minifig.part.Part)}.
+	 * Test method for {@link PartUtility#calculateImageId(Part, Part, Part)} .
 	 */
 	@Test
 	public void testCalculateImageId() {
@@ -77,5 +75,5 @@ public class PartUtilityTest {
 		Assert.assertEquals("ImageId", "Minifig_H01_T00_L03", PartUtility.calculateImageId(PART1, null, PART3));
 		Assert.assertEquals("ImageId", "Minifig_H01_T02_L00", PartUtility.calculateImageId(PART1, PART2, null));
 		Assert.assertEquals("ImageId", "Minifig_H00_T00_L00", PartUtility.calculateImageId(null, null, null));
-		}
+	}
 }
