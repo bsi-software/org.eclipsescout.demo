40e93aa3f96f5121aa840e705cc8856cf022cd27
Jeremie Bresson <jbr@bsiag.com>
2013-03-20 00:28:36 +0100

    Change the packaging to eclipse-test-plugin

---
+++ minifigcreator/org.eclipsescout.demo.minifigcreator.client.test/src/org/eclipsescout/demo/minifigcreator/client/ui/forms/DesktopFormTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.eclipsescout.demo.minifigcreator.client.ui.forms;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link org.eclipsescout.demo.minifigcreator.client.ui.forms.DesktopForm}
+ * @author jbr
+ *
+ */
+public class DesktopFormTest {
+
+	/**
+	 * Test method.
+	 */
+	@Test
+	public void test() {
+		//TODO
+	}
+
+}
