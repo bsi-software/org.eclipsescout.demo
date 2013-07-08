2f5d975e7a5c585c7a918aa2595edf37462018b6
Matthias Zimmermann <matthiaszimmermanntest>
2013-07-09 00:40:02 +0200

    reset opening links to scout default

--- ibug/org.eclipsescout.demo.ibug.ui.rap/src/org/eclipsescout/demo/ibug/ui/rap/MobileStandaloneRwtEnvironment.java
+++ ibug/org.eclipsescout.demo.ibug.ui.rap/src/org/eclipsescout/demo/ibug/ui/rap/MobileStandaloneRwtEnvironment.java
@@ -10,21 +10,12 @@
  ******************************************************************************/
 package org.eclipsescout.demo.ibug.ui.rap;
 
-import org.eclipsescout.demo.ibug.client.ClientSession;
 import org.eclipse.scout.rt.ui.rap.mobile.AbstractMobileStandaloneRwtEnvironment;
-import org.eclipse.scout.rt.ui.rap.window.BrowserWindowHandler;
+import org.eclipsescout.demo.ibug.client.ClientSession;
 
 public class MobileStandaloneRwtEnvironment extends AbstractMobileStandaloneRwtEnvironment {
 
   public MobileStandaloneRwtEnvironment() {
     super(Activator.getDefault().getBundle(), ClientSession.class);
-  }
-
-  /* (non-Javadoc)
-   * @see org.eclipse.scout.rt.ui.rap.mobile.AbstractMobileStandaloneRwtEnvironment#createBrowserWindowHandler()
-   */
-  @Override
-  protected BrowserWindowHandler createBrowserWindowHandler() {
-    return new IBugMobileBrowserWindowHandler();
   }
 }
