9315a331778a4205eef3aa3e400e49a1048310a7
Jeremie Bresson <jbr@bsiag.com>
2013-08-01 17:57:40 +0200

    BahBah: renamed projects to org.eclipsescout.demo.bahbah.*

---
+++ bahbah/org.eclipsescout.demo.bahbah.server/src/org/eclipsescout/demo/bahbah/server/services/common/sql/DerbySqlService.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipsescout.demo.bahbah.server.services.common.sql;
+
+import org.eclipse.scout.rt.services.common.jdbc.AbstractDerbySqlService;
+import org.eclipse.scout.service.IService2;
+
+public class DerbySqlService extends AbstractDerbySqlService implements IService2{
+}
