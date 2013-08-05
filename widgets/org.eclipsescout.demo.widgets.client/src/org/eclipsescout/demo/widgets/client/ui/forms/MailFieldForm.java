724f323d52c4926d332c2cd43af5bc8f87fe66d3
Jeremie Bresson <jbr@bsiag.com>
2013-08-05 11:22:39 +0200

    Widgets: create maven build
    Remove dependency on javax.mail.internet
    
    New projects:
    * org.eclipsescout.demo.widgets.parent
    * org.eclipsescout.demo.widgets.ui.rap.product
    * org.eclipsescout.demo.widgets.ui.swing.product
    * org.eclipsescout.demo.widgets.ui.swt.product
    
    add maven nature to all projects

--- widgets/org.eclipsescout.demo.widgets.client/src/org/eclipsescout/demo/widgets/client/ui/forms/MailFieldForm.java
+++ widgets/org.eclipsescout.demo.widgets.client/src/org/eclipsescout/demo/widgets/client/ui/forms/MailFieldForm.java
@@ -10,12 +10,8 @@
  ******************************************************************************/
 package org.eclipsescout.demo.widgets.client.ui.forms;
 
-import java.io.InputStream;
+//TODO: check how maven should handle javax.mail.internet.MimeMessage.
 
-import javax.mail.internet.MimeMessage;
-
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.scout.commons.annotations.Order;
 import org.eclipse.scout.commons.exception.ProcessingException;
 import org.eclipse.scout.rt.client.ui.form.AbstractForm;
@@ -23,15 +19,14 @@ import org.eclipse.scout.rt.client.ui.form.AbstractFormHandler;
 import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractCloseButton;
 import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractLinkButton;
 import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
-import org.eclipse.scout.rt.client.ui.form.fields.mailfield.AbstractMailField;
 import org.eclipse.scout.rt.shared.TEXTS;
-import org.eclipsescout.demo.widgets.client.Activator;
 import org.eclipsescout.demo.widgets.client.ui.forms.MailFieldForm.MainBox.CloseButton;
 import org.eclipsescout.demo.widgets.client.ui.forms.MailFieldForm.MainBox.HTMLButton;
 import org.eclipsescout.demo.widgets.client.ui.forms.MailFieldForm.MainBox.HTMLWithAttachmentsButton;
 import org.eclipsescout.demo.widgets.client.ui.forms.MailFieldForm.MainBox.HTMLWithInnerImagesButton;
-import org.eclipsescout.demo.widgets.client.ui.forms.MailFieldForm.MainBox.MailField;
 import org.eclipsescout.demo.widgets.client.ui.forms.MailFieldForm.MainBox.TextButton;
+
+//import javax.mail.internet.MimeMessage;
 
 public class MailFieldForm extends AbstractForm implements IPageForm {
 
@@ -66,9 +61,9 @@ public class MailFieldForm extends AbstractForm implements IPageForm {
     return getFieldByClass(HTMLWithInnerImagesButton.class);
   }
 
-  public MailField getMailField() {
-    return getFieldByClass(MailField.class);
-  }
+//  public MailField getMailField() {
+//    return getFieldByClass(MailField.class);
+//  }
 
   public MainBox getMainBox() {
     return getFieldByClass(MainBox.class);
@@ -78,43 +73,43 @@ public class MailFieldForm extends AbstractForm implements IPageForm {
     return getFieldByClass(TextButton.class);
   }
 
-  public void loadFile(String fileName) throws ProcessingException {
-    try {
-      InputStream inStream = FileLocator.openStream(Activator.getDefault().getBundle(), new Path("resources/mails/" + fileName), true);
-      MimeMessage message = new MimeMessage(null, inStream);
-      getMailField().setValue(message);
-    }
-    catch (Exception e) {
-      throw new ProcessingException(null, e);
-    }
-  }
+//  public void loadFile(String fileName) throws ProcessingException {
+//    try {
+//      InputStream inStream = FileLocator.openStream(Activator.getDefault().getBundle(), new Path("resources/mails/" + fileName), true);
+//      MimeMessage message = new MimeMessage(null, inStream);
+//      getMailField().setValue(message);
+//    }
+//    catch (Exception e) {
+//      throw new ProcessingException(null, e);
+//    }
+//  }
 
   @Order(10.0)
   public class MainBox extends AbstractGroupBox {
 
-    @Order(10.0)
-    public class MailField extends AbstractMailField {
-
-      @Override
-      protected int getConfiguredGridH() {
-        return 20;
-      }
-
-      @Override
-      protected int getConfiguredGridW() {
-        return 2;
-      }
-
-      @Override
-      protected String getConfiguredLabel() {
-        return TEXTS.get("MailField");
-      }
-
-      @Override
-      protected boolean getConfiguredLabelVisible() {
-        return false;
-      }
-    }
+//    @Order(10.0)
+//    public class MailField extends AbstractMailField {
+//
+//      @Override
+//      protected int getConfiguredGridH() {
+//        return 20;
+//      }
+//
+//      @Override
+//      protected int getConfiguredGridW() {
+//        return 2;
+//      }
+//
+//      @Override
+//      protected String getConfiguredLabel() {
+//        return TEXTS.get("MailField");
+//      }
+//
+//      @Override
+//      protected boolean getConfiguredLabelVisible() {
+//        return false;
+//      }
+//    }
 
     @Order(20.0)
     public class TextButton extends AbstractLinkButton {
@@ -126,7 +121,12 @@ public class MailFieldForm extends AbstractForm implements IPageForm {
 
       @Override
       protected void execClickAction() throws ProcessingException {
-        loadFile("textMail.mail");
+//        loadFile("textMail.mail");
+      }
+
+      @Override
+      protected boolean getConfiguredEnabled() {
+        return false;
       }
     }
 
@@ -140,7 +140,12 @@ public class MailFieldForm extends AbstractForm implements IPageForm {
 
       @Override
       protected void execClickAction() throws ProcessingException {
-        loadFile("htmlMail.mail");
+//        loadFile("htmlMail.mail");
+      }
+
+      @Override
+      protected boolean getConfiguredEnabled() {
+        return false;
       }
     }
 
@@ -154,7 +159,12 @@ public class MailFieldForm extends AbstractForm implements IPageForm {
 
       @Override
       protected void execClickAction() throws ProcessingException {
-        loadFile("htmlMailWithInnerImages.mail");
+//        loadFile("htmlMailWithInnerImages.mail");
+      }
+
+      @Override
+      protected boolean getConfiguredEnabled() {
+        return false;
       }
     }
 
@@ -168,7 +178,12 @@ public class MailFieldForm extends AbstractForm implements IPageForm {
 
       @Override
       protected void execClickAction() throws ProcessingException {
-        loadFile("htmlMailWithAttachments.mail");
+//        loadFile("htmlMailWithAttachments.mail");
+      }
+
+      @Override
+      protected boolean getConfiguredEnabled() {
+        return false;
       }
     }
 
