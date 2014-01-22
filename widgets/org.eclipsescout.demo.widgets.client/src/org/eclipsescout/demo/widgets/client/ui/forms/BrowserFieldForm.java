5de0daaebb61439f11a22575302ddc4948fe0b48
Jeremie Bresson <jbr@bsiag.com>
2014-01-22 11:36:56 +0100

    Adjustments for Bug 426326 - Rap: Links in HtmlField must not open in the iframe
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=426326

--- widgets/org.eclipsescout.demo.widgets.client/src/org/eclipsescout/demo/widgets/client/ui/forms/BrowserFieldForm.java
+++ widgets/org.eclipsescout.demo.widgets.client/src/org/eclipsescout/demo/widgets/client/ui/forms/BrowserFieldForm.java
@@ -4,14 +4,18 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 package org.eclipsescout.demo.widgets.client.ui.forms;
 
+import java.io.InputStreamReader;
+
 import org.eclipse.scout.commons.annotations.Order;
 import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.commons.logger.IScoutLogger;
+import org.eclipse.scout.commons.logger.ScoutLogManager;
 import org.eclipse.scout.rt.client.ui.form.AbstractForm;
 import org.eclipse.scout.rt.client.ui.form.AbstractFormHandler;
 import org.eclipse.scout.rt.client.ui.form.fields.browserfield.AbstractBrowserField;
@@ -19,12 +23,15 @@ import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractCloseButton;
 import org.eclipse.scout.rt.client.ui.form.fields.button.AbstractLinkButton;
 import org.eclipse.scout.rt.client.ui.form.fields.groupbox.AbstractGroupBox;
 import org.eclipse.scout.rt.shared.TEXTS;
+import org.eclipse.scout.rt.shared.services.common.file.RemoteFile;
+import org.eclipsescout.demo.widgets.client.Activator;
 import org.eclipsescout.demo.widgets.client.ui.forms.BrowserFieldForm.MainBox.BrowserField;
 import org.eclipsescout.demo.widgets.client.ui.forms.BrowserFieldForm.MainBox.BsiagButton;
 import org.eclipsescout.demo.widgets.client.ui.forms.BrowserFieldForm.MainBox.CloseButton;
 import org.eclipsescout.demo.widgets.client.ui.forms.BrowserFieldForm.MainBox.EclipseScoutButton;
 
 public class BrowserFieldForm extends AbstractForm implements IPageForm {
+  private static final IScoutLogger LOG = ScoutLogManager.getLogger(BrowserFieldForm.class);
 
   public BrowserFieldForm() throws ProcessingException {
     super();
@@ -66,6 +73,17 @@ public class BrowserFieldForm extends AbstractForm implements IPageForm {
     return getFieldByClass(MainBox.class);
   }
 
+  private void loadFile(String simpleName) throws ProcessingException {
+    try {
+      RemoteFile remoteFile = new RemoteFile("simpleName", 0);
+      remoteFile.readData(new InputStreamReader(Activator.getDefault().getBundle().getResource("resources/html/" + simpleName).openStream()));
+      getBrowserField().setValue(remoteFile);
+    }
+    catch (Exception e) {
+      throw new ProcessingException("Html-Field can't load file ", e);
+    }
+  }
+
   @Order(10.0)
   public class MainBox extends AbstractGroupBox {
 
@@ -95,6 +113,11 @@ public class BrowserFieldForm extends AbstractForm implements IPageForm {
       @Override
       protected boolean getConfiguredScrollBarEnabled() {
         return true;
+      }
+
+      @Override
+      protected void execLocationChanged(String location, String path, boolean local) throws ProcessingException {
+        LOG.info("Location changed to " + location);
       }
     }
 
@@ -129,6 +152,20 @@ public class BrowserFieldForm extends AbstractForm implements IPageForm {
       protected void execClickAction() throws ProcessingException {
         getBrowserField().setValue(null);
         getBrowserField().setLocation("http://www.bsiag.com");
+      }
+    }
+
+    @Order(50.0)
+    public class CustomHtmlButton extends AbstractLinkButton {
+
+      @Override
+      protected String getConfiguredLabel() {
+        return TEXTS.get("CustomHtml");
+      }
+
+      @Override
+      protected void execClickAction() throws ProcessingException {
+        loadFile("BrowserFieldCustomHtml.html");
       }
     }
   }
