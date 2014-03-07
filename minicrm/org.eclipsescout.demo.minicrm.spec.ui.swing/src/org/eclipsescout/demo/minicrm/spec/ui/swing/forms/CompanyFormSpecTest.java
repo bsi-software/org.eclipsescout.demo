5c6a1b4fffdf05eb3398722b6363a11b65096b74
Adrian Sacchi <adrian.sacchi@bsiag.com>
2014-03-07 16:37:04 +0100

    bug 418309: [Spec] Generate a document with Scout GUI Model Properties dynamically at runtime as reference documentation or specification https://bugs.eclipse.org/bugs/show_bug.cgi?id=418309
    fixed minicrm spec
    
    Change-Id: I2b2001f415b4d844b2b48039dd8a9d56da715461
    Signed-off-by: Adrian Sacchi <adrian.sacchi@bsiag.com>

--- minicrm/org.eclipsescout.demo.minicrm.spec.ui.swing/src/org/eclipsescout/demo/minicrm/spec/ui/swing/forms/CompanyFormSpecTest.java
+++ minicrm/org.eclipsescout.demo.minicrm.spec.ui.swing/src/org/eclipsescout/demo/minicrm/spec/ui/swing/forms/CompanyFormSpecTest.java
@@ -1,15 +1,21 @@
 package org.eclipsescout.demo.minicrm.spec.ui.swing.forms;
 
-import org.eclipse.scout.commons.exception.ProcessingException;
-import org.eclipse.scout.rt.client.ui.form.IForm;
-import org.eclipsescout.demo.minicrm.client.ui.desktop.form.CompanyForm;
-import org.eclipsescout.demo.minicrm.spec.ui.swing.AbstractMiniCRMFormSpec;
+import java.util.List;
 
-public class CompanyFormSpecTest extends AbstractMiniCRMFormSpec {
+import org.eclipse.scout.commons.exception.ProcessingException;
+import org.eclipse.scout.rt.client.ui.form.FormListener;
+import org.eclipse.scout.rt.client.ui.form.IForm;
+import org.eclipse.scout.rt.spec.client.AbstractFormSpecTest;
+import org.eclipsescout.demo.minicrm.client.ui.desktop.form.CompanyForm;
+
+public class CompanyFormSpecTest extends AbstractFormSpecTest {
 
   @Override
-  protected IForm createAndStartForm() throws ProcessingException {
+  protected IForm createAndStartForm(List<FormListener> formListeners) throws ProcessingException {
     CompanyForm form = new CompanyForm();
+    for (FormListener listener : formListeners) {
+      form.addFormListener(listener);
+    }
     form.startNew();
     return form;
   }
