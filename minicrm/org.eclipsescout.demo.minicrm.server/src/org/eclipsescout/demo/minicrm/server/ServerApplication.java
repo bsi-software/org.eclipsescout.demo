25c0b134ef0c5f2d34bbc38d6191851b6cc12214
Ivan Motsch <ivan.motsch@bsiag.com>
2015-02-26 13:16:32 +0100

    eliminate Activator classes, load resources using local ResourceBase.class

--- minicrm/org.eclipsescout.demo.minicrm.server/src/org/eclipsescout/demo/minicrm/server/ServerApplication.java
+++ minicrm/org.eclipsescout.demo.minicrm.server/src/org/eclipsescout/demo/minicrm/server/ServerApplication.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -41,7 +41,7 @@ public class ServerApplication implements IApplication {
     scheduler.addJob(new LoadJobs());
     scheduler.start();
     Activator.getDefault().setScheduler(scheduler);
-    */
+     */
     logger.info("minicrm server initialized");
     return EXIT_OK;
   }
