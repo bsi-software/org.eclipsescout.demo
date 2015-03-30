4cbf606294bcde72dfccab3ec990c274506a0277
Matthias Villiger <Matthias.Villiger@bsiag.com>
2015-03-30 14:37:41 +0200

    Bug 458143 - Enable Scout for pure Java applications (without osgi)

--- bahbah/org.eclipsescout.demo.bahbah.shared/src/main/java/org/eclipsescout/demo/bahbah/shared/notification/RefreshBuddiesNotification.java
+++ bahbah/org.eclipsescout.demo.bahbah.shared/src/main/java/org/eclipsescout/demo/bahbah/shared/notification/RefreshBuddiesNotification.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -14,7 +14,7 @@ import org.eclipse.scout.rt.shared.services.common.clientnotification.AbstractCl
 import org.eclipse.scout.rt.shared.services.common.clientnotification.IClientNotification;
 
 /**
- * 
+ *
  */
 public class RefreshBuddiesNotification extends AbstractClientNotification {
   private static final long serialVersionUID = 1L;
